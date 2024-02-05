63150890f46a7c16e16af6441138f5d84c5a23a9
vladimir-ikryanov <vladimir.ikryanov@teamdev.com>
2024-02-05 11:05:35 +0200

    Show/hide window using global shortcuts

--- src-cpp/src/main.cc
+++ src-cpp/src/main.cc
@@ -4,21 +4,38 @@ using namespace molybden;
 
 void launch() {
   App::init([](std::shared_ptr<App> app) {
+    // Hide the dock icon and make the app a background app.
+    app->dock()->hide();
+
     auto browser = Browser::create(app);
-    
+
     // Hide all standard window buttons.
     browser->setWindowButtonVisible(WindowButtonType::kMinimize, false);
     browser->setWindowButtonVisible(WindowButtonType::kZoom, false);
     browser->setWindowButtonVisible(WindowButtonType::kClose, false);
-    
+
     // Hide window title and title bar.
     browser->setWindowTitleVisible(false);
     browser->setWindowTitlebarVisible(false);
-    
+
     // Display the window always on top of other windows.
     browser->setAlwaysOnTop(true);
 
     browser->loadUrl(app->baseUrl());
-    browser->show();
+
+    auto global_shortcuts = app->globalShortcuts();
+    // Register a global shortcut to show the browser window.
+    auto shortcut_show = Shortcut(KeyCode::V, KeyModifier::COMMAND_OR_CTRL | KeyModifier::SHIFT);
+    global_shortcuts->registerShortcut(shortcut_show, [browser](const Shortcut &) {
+      browser->show();
+    });
+
+    // Register a global shortcut to hide the browser window.
+    auto shortcut_hide = Shortcut(KeyCode::ESC);
+    global_shortcuts->registerShortcut(shortcut_hide, [browser](const Shortcut &) {
+      if (browser->isActive()) {
+        browser->hide();
+      }
+    });
   });
 }
