b93ffd23ae2c5349838a25e844089ed1bbd7f20d
vladimir-ikryanov <vladimir.ikryanov@teamdev.com>
2024-02-08 01:14:18 +0200

    Add tray icon

--- src-cpp/src/main_app.cc
+++ src-cpp/src/main_app.cc
@@ -2,12 +2,21 @@
 
 using namespace molybden;
 
-MainApp::MainApp(const std::shared_ptr<App>& app) : app_(app) {
-// Hide the dock icon and make the app a background app.
+MainApp::MainApp(const std::shared_ptr<App> &app) : app_(app) {
+  auto tray = Tray::create(app);
+  tray->setImage(app->getPath(PathKey::kAppResources) + "/imageTemplate.png");
+  tray->setMenu(CustomMenu::create(
+      {
+          menu::Item("Quit", [app](const CustomMenuItemActionArgs &args) {
+            app->quit();
+          })
+      }));
+
+  // Hide the dock icon and make the app a background app.
   app_->dock()->hide();
 
   browser_ = Browser::create(app);
-  browser_->onInjectJs = [this](const InjectJsArgs& args, InjectJsAction action) {
+  browser_->onInjectJs = [this](const InjectJsArgs &args, InjectJsAction action) {
     args.window->putProperty("pasteInFrontApp", [this](std::string text) {
       paste(text);
     });
