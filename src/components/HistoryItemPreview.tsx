4cdf0120521f3cd62ec08f4b665b3d33986c1cb1
vladimir-ikryanov <vladimir.ikryanov@teamdev.com>
2024-02-14 13:42:47 +0200

    Add help bar in preview

--- src/components/HistoryItemPreview.tsx
+++ src/components/HistoryItemPreview.tsx
@@ -1,6 +1,8 @@
 import '../App.css';
 import {TabsContent} from "@/components/ui/tabs";
 import {ScrollArea} from "@/components/ui/scroll-area";
+import {Button} from "@/components/ui/button"
+import {CornerDownLeft, Delete} from "lucide-react";
 
 type HistoryItemPreviewProps = {
   index: number
@@ -16,6 +18,21 @@ export default function HistoryItemPreview({index, text}: HistoryItemPreviewProp
               <pre className="text-wrap text-sm">{text}</pre>
             </div>
           </ScrollArea>
+          <div className="grow"></div>
+          <div className="">
+            <div className="flex flex-row justify-end m-2 mt-0 text-neutral-600 text-sm">
+              <div className="flex flex-row">
+                <p className="pr-2">Paste to Clion</p>
+                <Button disabled={true} className="btn p-2 mr-3 h-6 rounded-sm bg-neutral-200">
+                  <CornerDownLeft className="h-4 w-4 text-foreground"/>
+                </Button>
+                <p className="pr-2">Delete</p>
+                <Button disabled={true} className="btn p-2 h-6 rounded-sm bg-neutral-200">
+                  <Delete className="h-4 w-4 text-foreground"/>
+                </Button>
+              </div>
+            </div>
+          </div>
         </div>
       </TabsContent>
   )
