4666f457a1be5271f8b5167d042ac421307e17dc
vladimir-ikryanov <vladimir.ikryanov@teamdev.com>
2024-02-15 01:35:48 +0200

    Add settings dialog

--- src/components/Preferences.tsx
+++ src/components/Preferences.tsx
@@ -1,32 +1,90 @@
 import '../App.css';
 import {Button} from "@/components/ui/button"
-import {Settings} from "lucide-react"
+import { Checkbox } from "@/components/ui/checkbox"
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group"
+import {CaseSensitive, Settings} from "lucide-react"
 import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet"
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 
 export default function Preferences() {
   return (
-      <Sheet>
-        <SheetTrigger>
+      <Dialog>
+        <DialogTrigger asChild>
           <Button variant="ghost">
             <Settings className="h-4 w-4"/>
           </Button>
-        </SheetTrigger>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your account
-              and remove your data from our servers.
-            </SheetDescription>
-          </SheetHeader>
-        </SheetContent>
-      </Sheet>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle></DialogTitle>
+            <DialogDescription className="pt-6 pb-4">
+              <div className="flex flex-col">
+                <div className="grid grid-cols-6 justify-center">
+                  <div className="text-right col-span-2 mt-auto mb-auto mr-8">
+                    <p>Startup</p>
+                  </div>
+                  <div className="col-span-4 flex space-x-2">
+                    <Checkbox id="startup" className="mt-auto mb-auto rounded"/>
+                    <label
+                        htmlFor="startup"
+                        className="font-medium leading-none peer-disabled:cursor-not-allowed mt-auto mb-auto">
+                      Launch Clipboard at login
+                    </label>
+                  </div>
+                </div>
+
+                <div className="grid grid-cols-6 justify-center mt-6">
+                  <div className="text-right col-span-2 mt-auto mb-auto mr-8">
+                    <p>Menu bar icon</p>
+                  </div>
+                  <div className="col-span-4 flex space-x-2">
+                    <Checkbox id="tray-icon" className="mt-auto mb-auto rounded"/>
+                    <label
+                        htmlFor="tray-icon"
+                        className="font-medium leading-none peer-disabled:cursor-not-allowed mt-auto mb-auto">
+                      Show Clipboard in menu bar
+                    </label>
+                  </div>
+                </div>
+
+                <div className="grid grid-cols-6 justify-center mt-6">
+                  <div className="text-right col-span-2 mt-auto mb-auto mr-8">
+                    <p>Appearance</p>
+                  </div>
+                  <div className="col-span-4">
+                    <ToggleGroup type="single" defaultValue="a" size={"lg"}>
+                      <ToggleGroupItem value="a">Light</ToggleGroupItem>
+                      <ToggleGroupItem value="b">Dark</ToggleGroupItem>
+                      <ToggleGroupItem value="c">System</ToggleGroupItem>
+                    </ToggleGroup>
+                  </div>
+                </div>
+
+                <div className="grid grid-cols-6 justify-center mt-6">
+                  <div className="text-right col-span-2 mt-auto mb-auto mr-8">
+                    <p>Text size</p>
+                  </div>
+                  <div className="col-span-4">
+                    <ToggleGroup type="single" defaultValue="a" size={"lg"}>
+                      <ToggleGroupItem value="a">
+                        <CaseSensitive className="h-6 w-6"/>
+                      </ToggleGroupItem>
+                      <ToggleGroupItem value="b">
+                        <CaseSensitive className="h-8 w-8"/>
+                      </ToggleGroupItem>
+                    </ToggleGroup>
+                  </div>
+                </div>
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
   )
 }
