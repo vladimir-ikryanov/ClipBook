2ae1fb43aa05abfbdb4ab6d4b660bfb77a7efd72
vladimir-ikryanov <vladimir.ikryanov@teamdev.com>
2024-06-24 13:22:55 +0300

    Fix horizontal scrolling in preview

--- src/components/HistoryItemPreview.tsx
+++ src/components/HistoryItemPreview.tsx
@@ -1,8 +1,5 @@
 import '../App.css';
 import {TabsContent} from "@/components/ui/tabs";
-import {ScrollArea} from "@/components/ui/scroll-area";
-import {Button} from "@/components/ui/button"
-import {ArrowBigDown, ArrowBigUp, ArrowDown, ArrowUp, CornerDownLeft, Delete} from "lucide-react";
 
 type HistoryItemPreviewProps = {
   index: number
@@ -14,9 +11,9 @@ export default function HistoryItemPreview(props: HistoryItemPreviewProps) {
   return (
       <TabsContent value={props.index.toString()} className="m-0">
         <div className="flex flex-col h-screen p-0 m-0">
-          <ScrollArea className="h-screen m-2">
-            <pre className="text-wrap text-sm m-4">{props.text}</pre>
-          </ScrollArea>
+          <textarea
+              className="h-full p-2 mt-2 mb-2 ml-2 mr-1 bg-secondary border-none outline-none resize-none font-mono text-sm"
+              value={props.text} readOnly={true}/>
         </div>
       </TabsContent>
   )
