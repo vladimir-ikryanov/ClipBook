2da6bde5a036be745c3a2a189a5f369df5ad7680
vladimir-ikryanov <vladimir.ikryanov@teamdev.com>
2024-07-11 02:45:43 +0300

    Add actions item with menu

---
+++ src/components/Actions.tsx
@@ -0,0 +1,71 @@
+import '../App.css';
+import {Button} from "@/components/ui/button";
+
+import * as React from "react"
+import {
+  DeleteIcon,
+  Edit2Icon,
+  Edit3Icon,
+  EditIcon,
+  MoreHorizontal,
+  SearchIcon,
+  TrashIcon
+} from "lucide-react"
+
+import {
+  Command,
+  CommandEmpty,
+  CommandInput,
+  CommandItem,
+  CommandList, CommandSeparator, CommandShortcut,
+} from "@/components/ui/command"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+type ActionsProps = {}
+
+export default function Actions(props: ActionsProps) {
+  const [open, setOpen] = React.useState(false)
+
+  return (
+      <DropdownMenu open={open} onOpenChange={setOpen}>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" size="sm">
+            <MoreHorizontal/>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-[300px]">
+          <Command>
+            <CommandList>
+              <CommandItem>
+                <Edit3Icon className="mr-2 h-4 w-4"/>
+                <span>Edit content...</span>
+                <CommandShortcut>⌘E</CommandShortcut>
+              </CommandItem>
+              <CommandItem>
+                <SearchIcon className="mr-2 h-4 w-4"/>
+                <span>Search...</span>
+                <CommandShortcut>⌘F</CommandShortcut>
+              </CommandItem>
+              <CommandItem>
+                <TrashIcon className="mr-2 h-4 w-4"/>
+                <span>Delete</span>
+                <CommandShortcut>⌘⌫</CommandShortcut>
+              </CommandItem>
+              <CommandItem>
+                <TrashIcon className="mr-2 h-4 w-4"/>
+                <span>Delete all...</span>
+                <CommandShortcut>⌘⌫</CommandShortcut>
+              </CommandItem>
+              <CommandSeparator/>
+              <CommandEmpty>No results found.</CommandEmpty>
+            </CommandList>
+            <CommandInput placeholder="Type a command or search..." autoFocus={true}/>
+          </Command>
+        </DropdownMenuContent>
+      </DropdownMenu>
+  )
+}
--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -7,6 +7,7 @@ import {
   Delete
 } from "lucide-react";
 import React from "react";
+import Actions from "@/components/Actions";
 
 type StatusBarProps = {
   appName: string
@@ -15,47 +16,41 @@ type StatusBarProps = {
 export default function StatusBar(props: StatusBarProps) {
   return (
       <div
-          className="flex flex-row p-2 text-sm text-primary-foreground border-t-solid border-t-border border-t draggable">
-        <div className="flex flex-row cursor-default p-1">
-          <Button disabled={true} className="btn p-0 mr-1 h-6 w-6 rounded bg-card">
-            <ChevronUp className="h-5 w-5 text-card-foreground"/>
-          </Button>
-          <Button disabled={true} className="btn p-0 h-6 w-6 rounded bg-card">
-            <ChevronDown className="h-5 w-5 text-card-foreground"/>
-          </Button>
-          <div className="flex items-center">
-            <p className="pl-2 pr-2">Navigate</p>
+          className="flex items-center justify-between p-2 border-t-solid border-t-border border-t draggable">
+        <div className="flex space-x-2 text-sm text-primary-foreground">
+          <div className="flex flex-row cursor-default p-1">
+            <Button disabled={true} className="btn p-0 mr-1 h-6 w-6 rounded bg-card">
+              <ChevronUp className="h-5 w-5 text-card-foreground"/>
+            </Button>
+            <Button disabled={true} className="btn p-0 h-6 w-6 rounded bg-card">
+              <ChevronDown className="h-5 w-5 text-card-foreground"/>
+            </Button>
+            <div className="flex items-center">
+              <p className="px-2">Navigate</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row cursor-default p-1">
+            <Button disabled={true} className="btn p-0 h-6 w-6 rounded bg-card">
+              <CornerDownLeft className="h-4 w-4 text-card-foreground"/>
+            </Button>
+            <div className="flex items-center">
+              <p className="px-2">Paste to {props.appName}</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row cursor-default p-1">
+            <Button disabled={true} className="btn px-2 h-6 rounded bg-card">
+              <span className="text-sm font-mono text-card-foreground">esc</span>
+            </Button>
+            <div className="flex items-center">
+              <p className="px-2">Close</p>
+            </div>
           </div>
         </div>
 
-        <div className="flex flex-row cursor-default p-1">
-          <Button disabled={true} className="btn p-0 h-6 w-6 rounded bg-card">
-            <CornerDownLeft className="h-4 w-4 text-card-foreground"/>
-          </Button>
-          <div className="flex items-center">
-            <p className="pl-2 pr-4">Paste to {props.appName}</p>
-          </div>
-        </div>
-
-        <div className="flex flex-row cursor-default p-1">
-          <Button disabled={true} className="btn p-0 mr-1 h-6 w-7 rounded bg-card">
-            <Command className="h-4 w-6 text-card-foreground"/>
-          </Button>
-          <Button disabled={true} className="btn p-0 h-6 w-7 rounded bg-card">
-            <Delete className="h-4 w-6 text-card-foreground"/>
-          </Button>
-          <div className="flex items-center">
-            <p className="pl-2 pr-4">Delete</p>
-          </div>
-        </div>
-
-        <div className="flex flex-row cursor-default p-1">
-          <Button disabled={true} className="btn px-2 h-6 rounded bg-card">
-            <span className="text-sm font-mono text-card-foreground">esc</span>
-          </Button>
-          <div className="flex items-center">
-            <p className="pl-2 pr-4">Close</p>
-          </div>
+        <div className="flex space-x-2">
+          <Actions/>
         </div>
       </div>
   )
