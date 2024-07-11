dcc7efa775dac4b3d39fa7912b80c74f73252758
vladimir-ikryanov <vladimir.ikryanov@teamdev.com>
2024-07-11 12:25:54 +0300

    Fix style

--- src/components/Actions.tsx
+++ src/components/Actions.tsx
@@ -3,11 +3,13 @@ import {Button} from "@/components/ui/button";
 
 import * as React from "react"
 import {
+  ArrowBigUp,
+  CommandIcon,
   DeleteIcon,
   Edit2Icon,
   Edit3Icon,
   EditIcon,
-  MoreHorizontal,
+  MoreHorizontal, PanelRightClose, PanelRightOpen,
   SearchIcon,
   TrashIcon
 } from "lucide-react"
@@ -24,41 +26,98 @@ import {
   DropdownMenuContent,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import {KeyboardEvent} from "react";
 
 type ActionsProps = {}
 
 export default function Actions(props: ActionsProps) {
   const [open, setOpen] = React.useState(false)
 
+  const handleKeyDown = (e: KeyboardEvent) => {
+    // Prevent leaving the keyboard event from the Actions menu.
+    e.stopPropagation()
+  }
+
   return (
       <DropdownMenu open={open} onOpenChange={setOpen}>
         <DropdownMenuTrigger asChild>
-          <Button variant="ghost" size="sm">
-            <MoreHorizontal/>
+          <Button variant="ghost" className="p-1 h-8">
+            <p className="px-2">Actions</p>
+            <div className="flex h-6 w-6 p-1.5 rounded bg-card">
+              <CommandIcon className="h-3 w-3 text-card-foreground"/>
+            </div>
+            <div className="flex h-6 w-6 ml-1 rounded bg-card justify-center items-center">
+              <span className="font-mono text-sm text-card-foreground">K</span>
+            </div>
           </Button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent align="end" className="w-[300px]">
+        <DropdownMenuContent align="end" className="w-[300px]" onKeyDown={handleKeyDown}>
           <Command>
             <CommandList>
               <CommandItem>
                 <Edit3Icon className="mr-2 h-4 w-4"/>
                 <span>Edit content...</span>
-                <CommandShortcut>⌘E</CommandShortcut>
+                <CommandShortcut className="flex flex-row">
+                  <div className="flex h-6 w-6 p-1.5 rounded bg-card">
+                    <CommandIcon className="h-3 w-3 text-card-foreground"/>
+                  </div>
+                  <div className="flex h-6 w-6 ml-1 rounded bg-card justify-center items-center">
+                    <span className="font-mono text-sm text-card-foreground">E</span>
+                  </div>
+                </CommandShortcut>
               </CommandItem>
               <CommandItem>
                 <SearchIcon className="mr-2 h-4 w-4"/>
                 <span>Search...</span>
-                <CommandShortcut>⌘F</CommandShortcut>
+                <CommandShortcut className="flex flex-row">
+                  <div className="flex h-6 w-6 p-1.5 rounded bg-card">
+                    <CommandIcon className="h-3 w-3 text-card-foreground"/>
+                  </div>
+                  <div className="flex h-6 w-6 ml-1 rounded bg-card justify-center items-center">
+                    <span className="font-mono text-sm text-card-foreground">F</span>
+                  </div>
+                </CommandShortcut>
+              </CommandItem>
+              <CommandItem>
+                <PanelRightClose className="mr-2 h-4 w-4"/>
+                <span>Hide Preview panel</span>
+                <CommandShortcut className="flex flex-row">
+                  <div className="flex h-6 w-6 p-1.5 rounded bg-card">
+                    <CommandIcon className="h-3 w-3 text-card-foreground"/>
+                  </div>
+                  <div className="flex h-6 w-6 ml-1 rounded bg-card justify-center items-center">
+                    <span className="font-mono text-sm text-card-foreground">P</span>
+                  </div>
+                </CommandShortcut>
               </CommandItem>
               <CommandItem>
                 <TrashIcon className="mr-2 h-4 w-4"/>
                 <span>Delete</span>
-                <CommandShortcut>⌘⌫</CommandShortcut>
+                <CommandShortcut className="flex flex-row">
+                  <div className="flex h-6 w-6 p-1.5 rounded bg-card">
+                    <CommandIcon className="h-3 w-3 text-card-foreground"/>
+                  </div>
+                  <div className="flex h-6 w-6 pt-0.5 ml-1 rounded bg-card justify-center items-center">
+                    <DeleteIcon className="h-4 w-4 text-card-foreground"/>
+                  </div>
+                </CommandShortcut>
               </CommandItem>
               <CommandItem>
                 <TrashIcon className="mr-2 h-4 w-4"/>
                 <span>Delete all...</span>
-                <CommandShortcut>⌘⌫</CommandShortcut>
+                <CommandShortcut className="flex flex-row">
+                  <div className="flex h-6 w-6 p-1.5 rounded bg-card">
+                    <CommandIcon className="h-3 w-3 text-card-foreground"/>
+                  </div>
+                  <div
+                      className="flex h-6 w-6 pt-0.5 ml-1 rounded bg-card justify-center items-center">
+                    <ArrowBigUp className="h-4 w-4 text-card-foreground"/>
+                  </div>
+                  <div
+                      className="flex h-6 w-6 pt-0.5 ml-1 rounded bg-card justify-center items-center">
+                    <DeleteIcon className="h-4 w-4 text-card-foreground"/>
+                  </div>
+                </CommandShortcut>
               </CommandItem>
               <CommandEmpty>No results found.</CommandEmpty>
             </CommandList>
--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -2,7 +2,7 @@ import '../App.css';
 import {Button} from "@/components/ui/button";
 import {
   ChevronDown,
-  ChevronUp, Command,
+  ChevronUp, Command, CommandIcon,
   CornerDownLeft,
   Delete
 } from "lucide-react";
@@ -16,37 +16,32 @@ type StatusBarProps = {
 export default function StatusBar(props: StatusBarProps) {
   return (
       <div
-          className="flex items-center justify-between p-2 border-t-solid border-t-border border-t draggable">
-        <div className="flex space-x-2 text-sm text-primary-foreground">
-          <div className="flex flex-row cursor-default p-1">
-            <Button disabled={true} className="btn p-0 mr-1 h-6 w-6 rounded bg-card">
-              <ChevronUp className="h-5 w-5 text-card-foreground"/>
-            </Button>
-            <Button disabled={true} className="btn p-0 h-6 w-6 rounded bg-card">
-              <ChevronDown className="h-5 w-5 text-card-foreground"/>
-            </Button>
-            <div className="flex items-center">
-              <p className="px-2">Navigate</p>
+          className="flex items-center justify-between p-2 border-t-solid border-t-border border-t">
+        <div className="flex space-x-1 text-sm text-primary-foreground">
+          <Button disabled={true} variant="ghost" className="p-1 h-8">
+            <div className="flex h-6 w-6 p-1.5 rounded bg-card">
+              <ChevronUp className="h-3 w-3 text-card-foreground"/>
             </div>
-          </div>
+            <div className="flex h-6 w-6 p-1.5 ml-1 rounded bg-card">
+              <ChevronDown className="h-3 w-3 text-card-foreground"/>
+            </div>
+            <p className="px-2">Navigate</p>
+          </Button>
 
-          <div className="flex flex-row cursor-default p-1">
-            <Button disabled={true} className="btn p-0 h-6 w-6 rounded bg-card">
-              <CornerDownLeft className="h-4 w-4 text-card-foreground"/>
-            </Button>
-            <div className="flex items-center">
-              <p className="px-2">Paste to {props.appName}</p>
-            </div>
-          </div>
 
-          <div className="flex flex-row cursor-default p-1">
-            <Button disabled={true} className="btn px-2 h-6 rounded bg-card">
-              <span className="text-sm font-mono text-card-foreground">esc</span>
-            </Button>
-            <div className="flex items-center">
-              <p className="px-2">Close</p>
+          <Button disabled={true} variant="ghost" className="p-1 h-8">
+            <div className="flex h-6 w-6 p-1.5 rounded bg-card">
+              <CornerDownLeft className="h-3 w-3 text-card-foreground"/>
             </div>
-          </div>
+            <p className="px-2">Paste to {props.appName}</p>
+          </Button>
+
+          <Button disabled={true} variant="ghost" className="p-1 h-8">
+            <div className="flex h-6 w-10 rounded bg-card justify-center items-center">
+              <span className="font-mono text-sm text-card-foreground">esc</span>
+            </div>
+            <p className="px-2">Close</p>
+          </Button>
         </div>
 
         <div className="flex space-x-2">
