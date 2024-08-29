be68cc9b9fb1b54748219416bc0f4107d191d342
vladimir-ikryanov <vladimir.ikryanov@teamdev.com>
2024-08-30 00:47:16 +0300

    Fix layout in search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -41,14 +41,14 @@ export default function SearchBar(props: SearchBarProps) {
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                    fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
                    strokeLinejoin="round"
-                   className="lucide lucide-search absolute left-3 top-2.5 h-5 w-5 text-secondary-foreground">
+                   className="lucide lucide-search absolute left-2 top-2.5 h-5 w-5 text-secondary-foreground">
                 <circle cx="11" cy="11" r="8"/>
                 <path d="m21 21-4.3-4.3"/>
               </svg>
             </div>
             <Input placeholder="Type to search..."
                    value={props.searchQuery}
-                   className="w-full pl-11 text-lg placeholder:text-secondary-foreground border-none"
+                   className="w-full pl-10 text-lg placeholder:text-secondary-foreground border-none"
                    onChange={handleOnChange}
                    onKeyDown={handleKeyDown}
                    ref={props.searchFieldRef}
