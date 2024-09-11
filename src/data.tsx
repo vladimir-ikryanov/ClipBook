2d5fdfb5c5a28072d162c6c01b04df246e704beb
vladimir-ikryanov <vladimir.ikryanov@teamdev.com>
2024-09-11 12:00:14 +0300

    Remove unused code

--- src/data.tsx
+++ src/data.tsx
@@ -23,29 +23,69 @@ let sortType = SortHistoryType.TimeOfLastCopy;
 
 loadSettings()
 await loadHistory()
+// await loadHistoryForPromo()
 
-// For testing purposes only!
-function generateRandomWord(length: number): string {
-  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
-  let result = ''
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length)
-    result += characters.charAt(randomIndex)
-  }
-  return result
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 // noinspection JSUnusedLocalSymbols
-function generateHistoryForTesting(size: number, wordLength: number): Clip[] {
-  const array: Clip[] = []
-  for (let i = 0; i < size; i++) {
-    array.push(new Clip(generateRandomWord(wordLength), "ClipBook"))
-  }
-  return array
+async function loadHistoryForPromo() {
+  await deleteAllClips()
+  await addClip(new Clip("Get it. Try it. Use it if you like it.", "/Applications/Arc.app"))
+  await sleep(500)
+  await addClip(new Clip("<section class=\"section-sm text-center\">\n" +
+      "  <div class=\"container\">\n" +
+      "    <div class=\"row justify-center\">\n" +
+      "      <div class=\"sm:col-10 md:col-8 lg:col-6\">\n" +
+      "        <span\n" +
+      "          class=\"text-[8rem] block font-bold text-dark dark:text-darkmode-dark\">\n" +
+      "          404\n" +
+      "        </span>\n" +
+      "        <h1 class=\"h2 mb-4\">Page not found</h1>\n" +
+      "        <div class=\"content\">\n" +
+      "          <p>\n" +
+      "            The page you are looking for might have been removed, had its name\n" +
+      "            changed, or is temporarily unavailable.\n" +
+      "          </p>\n" +
+      "        </div>\n" +
+      "        <a\n" +
+      "          href=\"{{ site.BaseURL | relLangURL }}\"\n" +
+      "          class=\"btn btn-primary mt-8\">\n" +
+      "          Back to home\n" +
+      "        </a>\n" +
+      "      </div>\n" +
+      "    </div>\n" +
+      "  </div>\n" +
+      "</section>", "/Applications/Visual Studio Code.app"))
+  await sleep(500)
+  await addClip(new Clip("john.doe@mail.com", "/System/Applications/Mail.app"))
+  await sleep(500)
+  await addClip(new Clip("rgb(255 100 3 / 80%)", "/Applications/Arc.app"))
+  await sleep(500)
+  await addClip(new Clip("hsl(330 100% 50%)", "/Applications/Arc.app"))
+  await sleep(500)
+  await addClip(new Clip("#65a30d", "/Applications/Arc.app"))
+  await sleep(500)
+  await addClip(new Clip("https://clipbook.app", "/Applications/Arc.app"))
+  await sleep(500)
+  await addClip(new Clip("🔒 All data is securely stored on your Mac and never leave it", "/System/Applications/Notes.app"))
+  await sleep(500)
+  await addClip(new Clip("✏️ Edit and preview history items", "/System/Applications/Notes.app"))
+  await sleep(500)
+  await addClip(new Clip("⭐️ Add items to favorites", "/System/Applications/Notes.app"))
+  await sleep(500)
+  await addClip(new Clip("🔎 Type to search", "/System/Applications/Notes.app"))
+  await sleep(500)
+  await addClip(new Clip("⌛️ Unlimited clipboard history", "/System/Applications/Notes.app"))
+  await sleep(500)
+  await addClip(new Clip("Keep everything you copy and quickly access it whenever you need it", "/System/Applications/Notes.app"))
+  await sleep(500)
+  await addClip(new Clip("Clipboard history app for your Mac", "/System/Applications/Notes.app"))
+  history = await getAllClips()
 }
 
 async function loadHistory() {
-  // history = generateHistoryForTesting(10024, 128)
   history = await getAllClips()
   if (localStorage.getItem("history")) {
     let items: HistoryItem[] = JSON.parse(localStorage.getItem("history")!)
