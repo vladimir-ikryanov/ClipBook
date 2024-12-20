3c6cbd0ef55be4e132d2df2c3fdd489bb0fd5306
vladimir-ikryanov <vladimir.ikryanov@teamdev.com>
2024-12-20 15:35:09 +0200

    Fix merge icon in Actions menu

--- src/app/Actions.tsx
+++ src/app/Actions.tsx
@@ -261,7 +261,19 @@ export default function Actions(props: ActionsProps) {
               {
                   canShowMergeItems() &&
                   <CommandItem onSelect={handleMerge}>
-                    <CopyIcon className="mr-2 h-4 w-4"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                         viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"
+                         strokeLinecap="round" strokeLinejoin="round"
+                         className="lucide lucide-unfold-vertical mr-2 h-4 w-4">
+                      <path d="M12 22v-6"/>
+                      <path d="M12 8V2"/>
+                      <path d="M4 12H2"/>
+                      <path d="M10 12H8"/>
+                      <path d="M16 12h-2"/>
+                      <path d="M22 12h-2"/>
+                      <path d="m15 5-3 3-3-3"/>
+                      <path d="m15 19-3-3-3 3"/>
+                    </svg>
                     <span>Merge {getMultipleItemsIndicator()}</span>
                   </CommandItem>
               }
