f5ab294a4e1311a76b22ea2cdda5d8017749f02b
vladimir-ikryanov <vladimir.ikryanov@teamdev.com>
2025-02-19 10:54:49 +0200

    Play sound by default

--- src-cpp/src/app_settings_mac.mm
+++ src-cpp/src/app_settings_mac.mm
@@ -941,7 +941,7 @@ bool AppSettingsMac::shouldPlaySoundOnCopy() {
   if ([defaults objectForKey:prefPlaySoundOnCopy] != nil) {
     return [defaults boolForKey:prefPlaySoundOnCopy];
   }
-  return false;
+  return true;
 }
 
 void AppSettingsMac::saveAlwaysDisplay(bool display) {
